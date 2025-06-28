3a24c7a2fe23888d017e4e2d07c0eb6d48f3d4e3
neverovnikita <153305920+neverovnikita@users.noreply.github.com>
2025-06-29 00:06:17 +0700

    Initial commit

---
+++ src/test/java/com/neverov/tasktracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neverov.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
